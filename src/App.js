423b804f7b5ca8b21439f244addb308f743fc500
Ethan Jackson <ethantjackson44@gmail.com>
2021-07-04 16:55:35 -0700

    initialize repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MuscleGroupSelectionPage from './pages/MuscleGroupSelectionPage';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <MuscleGroupSelectionPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/MuscleGroupSelectionPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MuscleGroupSelector from '../components/selectors/msucles/MuscleGroupSelector'
+
+const MuscleGroupSelectionPage = () => {
+	return (
+		<MuscleGroupSelector />
+	)
+}
+
+export default MuscleGroupSelectionPage
